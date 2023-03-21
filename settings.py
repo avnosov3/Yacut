7ec127c6f3508c26d88ce130292c0eb3117337d2
Artem Nosov <nosov1995@gmail.com>
2023-03-21 22:05:43 +0100

    1.4 try

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
 
 
 ORIGINAL = 'original'
